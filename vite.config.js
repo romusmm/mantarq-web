c570b8978b7478dcea40f774454fc8490fdef15b
romusmm <rmsalazarm8@gmail.com>
2025-08-15 18:20:51 -0500

    correct Vite

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "node:path";
-import { vitePrerenderPlugin } from "vite-prerender-plugin";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePrerenderPlugin({
-      renderTarget: "#root", // donde se monta tu App en index.html
-      prerenderScript: resolve(__dirname, "prerender.jsx"),
-      additionalPrerenderRoutes: [
-        "/", "/historia/", "/servicios/", "/faq/", "/contacto/", "/404.html"
-      ],
-      // previewMiddlewareFallback: '/index.html' // por defecto
-    }),
-  ],
-  base: "/", // con dominio propio o GH Pages por dominio, deja '/'
+  plugins: [react()],
+  // 👇 usa la base del repositorio mientras estés en Project Pages
+  base: "/mantarq-web/",
   resolve: { dedupe: ["react", "react-dom"] },
 });
