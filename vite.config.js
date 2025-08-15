723ee826a97fca3d7710e23d8a46ed34e18bedfd
romusmm <rmsalazarm8@gmail.com>
2025-08-15 16:26:26 -0500

    fix(ci): workflow YAML válido para Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Si TU REPO se llama "mantarq-web", usa base: '/mantarq-web/'
-// Si tu repo es "tuusuario.github.io", usa base: '/'
 export default defineConfig({
   plugins: [react()],
-  base: '/mantarq-web/',
+  base: '/mantarq-web/',     // cámbialo si tu repo tiene otro nombre
   resolve: { dedupe: ['react', 'react-dom'] },
 })
