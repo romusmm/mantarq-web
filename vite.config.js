57e2f36a63a8d5639ef1e0747e95cf064be88f17
romusmm <rmsalazarm8@gmail.com>
2026-06-02 17:55:47 -0500

    Change base path for custom domain in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // 👇 usa la base del repositorio mientras estés en Project Pages
-  base: "/mantarq-web/",
+  // 👇 Cambiado a la raíz para dominio personalizado
+  base: "/", 
   resolve: { dedupe: ["react", "react-dom"] },
 });
